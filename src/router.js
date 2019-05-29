c1c22d74497caecef69985d19d2ac89a0f0aa625
独行工匠 <515235416@qq.com>
2019-05-29 23:23:56 +0800

    新页面

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from './page/index.vue'
-
+import index from './lpj/index.vue'
+import esj from './esj/index.vue'
 
 Vue.use(Router)
 
@@ -13,8 +13,47 @@ export default new Router({
             component: index,
             meta: {
                 requireAuth: true,
-                title: '首页'
+                title: '零配件管理'
+            },
+            children: [{
+                    name: "lpj/add_lj",
+                    path: 'add_lj',
+                    meta: {
+                        requireAuth: true,
+                        title: '新增机型'
+                    },
+                    component: () =>
+                        import ('@/lpj/add_lj')
+            }, {
+                    name: "lpj/add_lpj",
+                    path: 'add_lpj',
+                    meta: {
+                        requireAuth: true,
+                        title: '新增零件'
+                    },
+                    component: () =>
+                        import ('@/lpj/add_lpj')
             }
+            ]
+
+        }, {
+            path: '/esj',
+            name: 'esj',
+            component: esj,
+            meta: {
+                requireAuth: true,
+                title: '二手机管理'
+            },
+            children: [{
+                name: "esj/add_esj",
+                path: 'add_esj',
+                meta: {
+                    requireAuth: true,
+                    title: '新增机型'
+                },
+                component: () =>
+                    import ('@/esj/add_esj')
+            }]
 
         }
 
